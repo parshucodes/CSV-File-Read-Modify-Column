81759b7392ccee3849f38d0bb309c4230909df7d
parshucodes <parshuram14234@gmail.com>
2024-02-19 18:29:28 +0530

    csvadd

---
+++ CsvFileReadModifyWrite.cs
@@ -0,0 +1,70 @@
+﻿namespace ReadModifyWriteCSVFile;
+
+interface IApplyInterface
+{
+    public List<string[]> ModifyColumn(string InputFilePath, int columnIndex);
+    public void saveToCsv(string OutputFilePath, List<string[]> data);
+}
+public class ReadModifyWriteCSVFile : IApplyInterface
+{
+    public string InputFilePath { get; set; }
+    public string OutputFilePath { get; set; }
+
+    public int columtomodify { get; set; }
+
+    public List<string[]> ModifyColumn(string InputFilePath, int columnIndex)
+    {
+        List<string[]> modifiedData = new List<string[]>();
+        try
+        {
+            
+            string lines;
+            using (StreamReader sr = new StreamReader(InputFilePath))
+            {
+                while ((lines = sr.ReadLine()) != null)
+                {
+                    string[] fields = lines.Split(',');
+                    if (columnIndex < fields.Length)
+                    {
+                        if (fields[columnIndex] == "male")
+                        {
+                            fields[columnIndex] = "Handsome";
+                        }
+                        else
+                        {
+                            fields[columnIndex] = "Beautiful";
+                        }
+                    }
+                    modifiedData.Add(fields);
+                }
+            }
+            
+        }
+        catch (System.Exception e)
+        {
+
+            Console.WriteLine("the file could not be read" + e.Message);
+        }
+        return modifiedData;
+
+    }
+
+    public void saveToCsv(string OutputFilePath, List<string[]> data)
+    {
+        try
+        {
+            using (StreamWriter wr = new StreamWriter(OutputFilePath, true))
+            {
+                foreach (var item in data)
+                {
+                    wr.WriteLine(string.Join(",", item));
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            
+            Console.WriteLine("unable to save the file" + e.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace ReadModifyWriteCSVFile;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadModifyWriteCSVFile readCSV = new ReadModifyWriteCSVFile();
+        readCSV.InputFilePath = @"c:\Users\parsh\Documents\Book1.csv";
+        readCSV.OutputFilePath = @"c:\Users\parsh\Documents\newout.csv";
+        readCSV.columtomodify = 3;
+
+        List<string[]> modifiedData = readCSV.ModifyColumn(readCSV.InputFilePath, readCSV.columtomodify);
+        readCSV.saveToCsv(readCSV.OutputFilePath, modifiedData);
+        Console.WriteLine("The CSV file has been modified and saved");
+    }
+}
